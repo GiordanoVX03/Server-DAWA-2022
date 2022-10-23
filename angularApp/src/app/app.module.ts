96578cf097f13856e97d29e5edf4c144ec7fdff0
GCalizaya <giordanovcc3@gmail.com>
2022-10-23 00:08:18 -0500

    v1 app angular

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductosComponent } from './productos/productos.component';
+import { AcercadeComponent } from './acercade/acercade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductosComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,
