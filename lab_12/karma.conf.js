1d565035c6c77a96f95220aa9fe1d7fc087944eb
GCalizaya <giordanovcc3@gmail.com>
2022-11-12 16:12:56 -0500

    lab 12 v1

--- lab_12/karma.conf.js
+++ lab_12/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/exm-s12'),
+      dir: require('path').join(__dirname, './coverage/lab_12'),
       subdir: '.',
       reporters: [
         { type: 'html' },
